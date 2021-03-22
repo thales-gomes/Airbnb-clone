c06ab9e06679b3948850a73ef1089b315576e9a6
Thales Gomes <thalessud@gmail.com>
2021-03-22 12:21:09 +0000

    sass package and Flat component created

---
+++ src/Flat.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Flat = () => {
+  return (
+    <div className="flat">
+      <img className="flat-picture" alt="flat"/>
+      <div className="flat-title">
+        PRICE - FLAT NAME
+      </div>
+    </div>
+  )
+}
+
+export default Flat;
