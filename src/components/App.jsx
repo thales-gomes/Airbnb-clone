b4d8a20731ee39b36936f17c8e2ee72af2296eb3
Thales Gomes <thalessud@gmail.com>
2021-03-22 17:07:23 +0000

    added flat seeds

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,83 @@ import "./App.scss";
 import Flat from "./Flat"
 
 class App extends React.Component {
+	state = {
+		flats: [
+			{
+			"id": 145,
+			"name": "Charm at the Steps of the Sacre Coeur/Montmartre",
+			"imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
+			"price": 164,
+			"priceCurrency": "EUR",
+			"lat": 48.884211,
+			"lng": 2.34689
+			},
+			{
+			"id": 148,
+			"name": "Trendy Apt in Buttes Montmartre",
+			"imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat2.jpg",
+			"price": 200,
+			"priceCurrency": "EUR",
+			"lat": 48.885707,
+			"lng": 2.343543
+			},
+			{
+			"id": 201,
+			"name": "Super 60m2 in trendy neighborhood!",
+			"imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat3.jpg",
+			"price": 150,
+			"priceCurrency": "EUR",
+			"lat": 48.885312,
+			"lng": 2.341225
+			},
+			{
+			"id": 205,
+			"name": "Splendide terrasse vue imprenable",
+			"imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat4.jpg",
+			"price": 115,
+			"priceCurrency": "EUR",
+			"lat": 48.88184,
+			"lng": 2.343371
+			},
+			{
+			"id": 210,
+			"name": "Superbe vue à 2 min du Sacré Coeur",
+			"imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat5.jpg",
+			"price": 135,
+			"priceCurrency": "EUR",
+			"lat": 48.888839,
+			"lng": 2.339208
+			},
+			{
+			"id": 211,
+			"name": "Bohemian and Chic in Paris",
+			"imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat6.jpg",
+			"price": 90,
+			"priceCurrency": "EUR",
+			"lat": 48.827855,
+			"lng": 2.350774
+			}
+			],
+	}
+
 	render() {
+		const { flats } = this.state;
+
 		return (
 			<div className="app">
 				<div className="main">
 					<input className="search" />
 					<div className="flats">
-					<Flat price={300} name="Great house"/>
-					<Flat price={150} name="Modern flat"/>
+						{
+							flats.map((flat, i) => {
+								const { name, price, id, imageUrl } = flat;
+								return <Flat
+									key={id}
+									price={price}
+									name={name}
+									imageUrl={imageUrl} />
+							})
+						}
 					</div>
 				</div>
 				<div className="map"></div>
--- src/components/Flat.jsx
+++ src/components/Flat.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import './Flat.scss'
 
 const Flat = (props) => {
-  const {price, name} = props;
+  const { price, name, imageUrl } = props;
+  console.log(imageUrl)
   return (
     <div className="flat">
-      <img className="flat-picture" alt="flat"/>
+      <img className="flat-picture" src={imageUrl} alt="flat"/>
       <div className="flat-title">
         {price} - {name}
       </div>
